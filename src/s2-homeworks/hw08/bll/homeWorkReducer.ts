c00b23f55012eca888761aaf62ed6bb424c9d9c7
slice of code <slice_of_code@mail.ru>
2024-02-03 19:50:45 +0400

    8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -8,8 +8,8 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
     switch (action.type) {
         case 'sort': { // by name
             const newState = [...state].sort((a, b) => {
-                if (a.name < b.name) return 1
-                else if (a.name > b.name) return -1
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
                 else return 0
             })
             return action.payload === 'up' ? newState : newState.reverse() // need to fix
