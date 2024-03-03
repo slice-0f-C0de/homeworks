a57cd8de2547dbfdbf93cc035134eccf99d60752
slice of code <slice_of_code@mail.ru>
2024-03-03 18:59:21 +0400

    9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -35,7 +35,7 @@ function Clock() {
     const stringDate = date?.toLocaleDateString() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = date.toDateString() || <br/> // пишут студенты
+    const stringDay = new Intl.DateTimeFormat("en", {weekday: "long"}).format() || <br/> // пишут студенты
     const stringMonth = date.getMonth() || <br/> // пишут студенты
 
     return (
